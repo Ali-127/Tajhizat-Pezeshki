d181c5c6ff42a037fa848e68e5ea83abfeafd7e3
Ali <alimehdizadeh127@gmail.com>
2026-07-06 12:08:15 +0330

    feat: Modify landing page template. Add dynamic urls for related links in landing page.

--- tp_app/urls.py
+++ tp_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import landing_view
+
+from .views import about_view, blog_view, landing_view, shop_view
 
 urlpatterns = [
-  path('', landing_view)
+  path('', landing_view, name='landing'),
+  path('shop/', shop_view, name='shop'),
+  path('about/', about_view, name='about'),
+  path('blog/', blog_view, name='blog'),
 ]
--- tp_app/views.py
+++ tp_app/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 
+from .models import BlogPost, Product
+
+
 def landing_view(request):
-  return render(request=request, template_name='landing.html')
+  active_products = Product.objects.filter(is_active=True).select_related(
+    'brand',
+    'category',
+  )
+  context = {
+    'special_products': active_products.order_by('-stock', '-created_at')[:5],
+    'latest_products': active_products.order_by('-created_at')[:5],
+    'latest_posts': BlogPost.objects.select_related('author').order_by('-created_at')[:3],
+  }
+  return render(request=request, template_name='landing.html', context=context)
 
 def shop_view(request):
-  return render(request=request, template_name='shop.html')
+  products = Product.objects.filter(is_active=True).select_related('brand', 'category')
+  return render(request=request, template_name='shop.html', context={'products': products})
 
-def abount_view(request):
+def about_view(request):
   return render(request=request, template_name='darabarema.html')
+
+def blog_view(request):
+  posts = BlogPost.objects.select_related('author').order_by('-created_at')
+  return render(request=request, template_name='weblog.html', context={'posts': posts})
 
 # def t_view(request):
 #   return render(request=request, template_name='shop.html')
